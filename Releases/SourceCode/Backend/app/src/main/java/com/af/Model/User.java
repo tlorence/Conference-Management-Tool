ecfd7dc1d5783362f6d4eae09407e7163dfe37d3
tlorence <tirone.lorence@gmail.com>
2021-06-30 00:30:56 +0530

    User modified

--- Releases/SourceCode/Backend/app/src/main/java/com/af/Model/User.java
+++ Releases/SourceCode/Backend/app/src/main/java/com/af/Model/User.java
@@ -10,99 +10,83 @@ public class User {
     private String name;
     private String password;
     private String role;
-    private byte[] file;
+    private String file;
     private String eduQualification;
     private String expertiseArea;
     private String paymentStatus;
-    private String approvalStatus;
 
-
-    public User(String email, String name, String password, String role, String file, String eduQualification, String expertiseArea, String paymentStatus, String approvalStatus) {
-    public void setEmail(String email) {
-
+    public User(String email, String name, String password, String role, String file, String eduQualification, String expertiseArea, String paymentStatus) {
         this.email = email;
-    }
-
-    public void setName(String name) {
         this.name = name;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
-    }
-
-    public void setRole(String role) {
         this.role = role;
-    }
-
-    public void setFile(byte[] file) {
         this.file = file;
-    }
-
-    public void setEduQualification(String eduQualification) {
         this.eduQualification = eduQualification;
-    }
-
-    public void setExpertiseArea(String expertiseArea) {
         this.expertiseArea = expertiseArea;
-
-        this.paymentStatus = "N/A";
-        this.approvalStatus = "pending";
-    }
-
-    public String getApprovalStatus() {
-        return approvalStatus;
-    }
-
-    public void setApprovalStatus(String approvalStatus) {
-        this.approvalStatus = approvalStatus;
-    }
-
-    public void setPaymentStatus(String paymentStatus) {
         this.paymentStatus = paymentStatus;
-
     }
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getPassword() {
         return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getRole() {
         return role;
     }
 
-    public byte[] getFile() {
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getFile() {
         return file;
+    }
+
+    public void setFile(String docPath) {
+        this.file = docPath;
     }
 
     public String getEduQualification() {
         return eduQualification;
     }
 
+    public void setEduQualification(String eduQualification) {
+        this.eduQualification = eduQualification;
+    }
+
     public String getExpertiseArea() {
         return expertiseArea;
+    }
+
+    public void setExpertiseArea(String expertiseArea) {
+        this.expertiseArea = expertiseArea;
     }
 
     public String getPaymentStatus() {
         return paymentStatus;
     }
 
-    public User(String email, String name, String password, String role, byte[] file, String eduQualification, String expertiseArea, String paymentStatus) {
-        this.email = email;
-        this.name = name;
-        this.password = password;
-        this.role = role;
-        this.file = file;
-        this.eduQualification = eduQualification;
-        this.expertiseArea = expertiseArea;
+    public void setPaymentStatus(String paymentStatus) {
         this.paymentStatus = paymentStatus;
     }
 }
