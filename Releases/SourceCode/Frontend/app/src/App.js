2c082a935de59065a3df6d1d95a7a2e055a9b661
Savvy9899 <pamuditha.rasanjana99@gmail.com>
2021-06-29 10:43:37 +0530

    front end add conference form done

--- Releases/SourceCode/Frontend/app/src/App.js
+++ Releases/SourceCode/Frontend/app/src/App.js
@@ -11,56 +11,36 @@ import Registration from "./Pages/Resregistration/Registration";
 import Login from "./Pages/Login/Login";
 
 function App() {
-    return (
-
-        <
-        BrowserRouter >
-        <
-        Switch >
-        <
-        Route path = "/admin" >
-        <
-        AdminDashboard / >
-        <
-        /Route> <
-        Route path = "/editor" >
-        <
-        EditorDashboard / >
-        <
-        /Route> <
-        Route path = "/reviewer" >
-        <
-        ReviewerDashboard / >
-        <
-        /Route> <
-        Route path = "/researcher" >
-        <
-        ResearcherDashboard / >
-        <
-        /Route> <
-        Route path = "/workshopPresenter" >
-        <
-        WorkshopPresenterDashboard / >
-        <
-        /Route> <
-        div >
-        <
-        Navbar / >
-        <
-        Route path = "/registration" >
-        <
-        Registration / >
-        <
-        /Route> <
-        Route path = "/login" >
-        <
-        Login / >
-        <
-        /Route> <
-        /div> <
-        /Switch> <
-        /BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/admin">
+          <AdminDashboard />
+        </Route>{" "}
+        <Route path="/editor">
+          <EditorDashboard />
+        </Route>{" "}
+        <Route path="/reviewer">
+          <ReviewerDashboard />
+        </Route>{" "}
+        <Route path="/researcher">
+          <ResearcherDashboard />
+        </Route>{" "}
+        <Route path="/workshopPresenter">
+          <WorkshopPresenterDashboard />
+        </Route>{" "}
+        <div>
+          <Navbar />
+          <Route path="/registration">
+            <Registration />
+          </Route>{" "}
+          <Route path="/login">
+            <Login />
+          </Route>{" "}
+        </div>{" "}
+      </Switch>{" "}
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ Releases/SourceCode/Frontend/app/src/services/conference.jsx
@@ -0,0 +1,2 @@
+import { Axios } from ".";
+import { USER } from "./endpoints";
