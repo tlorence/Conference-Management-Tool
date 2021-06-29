86a558b7c2ea6f7af0e6f073dc3bae052525ed90
wishva perera <vibushitha99@gamil.com>
2021-06-29 20:49:28 +0530

    tt

--- Releases/SourceCode/Backend/app/src/main/java/com/af/Model/User.java
+++ Releases/SourceCode/Backend/app/src/main/java/com/af/Model/User.java
@@ -10,19 +10,40 @@ public class User {
     private String name;
     private String password;
     private String role;
-    private String docPath;
+    private byte[] file;
     private String eduQualification;
     private String expertiseArea;
     private String paymentStatus;
 
-    public User(String email, String name, String password, String role, String docPath, String eduQualification, String expertiseArea, String paymentStatus) {
+    public void setEmail(String email) {
         this.email = email;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void setRole(String role) {
         this.role = role;
-        this.docPath = docPath;
+    }
+
+    public void setFile(byte[] file) {
+        this.file = file;
+    }
+
+    public void setEduQualification(String eduQualification) {
         this.eduQualification = eduQualification;
+    }
+
+    public void setExpertiseArea(String expertiseArea) {
         this.expertiseArea = expertiseArea;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
         this.paymentStatus = paymentStatus;
     }
 
@@ -30,63 +51,42 @@ public class User {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getRole() {
         return role;
     }
 
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getDocPath() {
-        return docPath;
-    }
-
-    public void setDocPath(String docPath) {
-        this.docPath = docPath;
+    public byte[] getFile() {
+        return file;
     }
 
     public String getEduQualification() {
         return eduQualification;
     }
 
-    public void setEduQualification(String eduQualification) {
-        this.eduQualification = eduQualification;
-    }
-
     public String getExpertiseArea() {
         return expertiseArea;
-    }
-
-    public void setExpertiseArea(String expertiseArea) {
-        this.expertiseArea = expertiseArea;
     }
 
     public String getPaymentStatus() {
         return paymentStatus;
     }
 
-    public void setPaymentStatus(String paymentStatus) {
+    public User(String email, String name, String password, String role, byte[] file, String eduQualification, String expertiseArea, String paymentStatus) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.role = role;
+        this.file = file;
+        this.eduQualification = eduQualification;
+        this.expertiseArea = expertiseArea;
         this.paymentStatus = paymentStatus;
     }
 }
