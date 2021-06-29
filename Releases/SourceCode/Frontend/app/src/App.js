b2d2f41fc6a3dee09efe24b3f6621c2a2273e827
Himantha_Amarathunga <Himanthaa@gmail.com>
2021-06-29 19:35:34 +0530

    Payment Page almost done

--- Releases/SourceCode/Frontend/app/src/App.js
+++ Releases/SourceCode/Frontend/app/src/App.js
@@ -1,64 +1,106 @@
 import "./App.css";
-import { Switch, BrowserRouter, Route } from "react-router-dom";
+
+import {Switch, BrowserRouter, Route} from "react-router-dom";
+
 import AdminDashboard from "./Pages/AdminDashboard/AdminDashboard";
+
 import Navbar from "./Pages/components/Navbar/Navbar";
+
 import Sidebar from "./Pages/components/sidebar/Sidebar";
+
 import EditorDashboard from "./Pages/EditerDashboard/EditorDashboard";
+
 import ReviewerDashboard from "./Pages/ReviewerDashboard/ReviewerDashboard";
+
 import ResearcherDashboard from "./Pages/ResearcherDashboard/ResearcherDashboard";
+
 import WorkshopPresenterDashboard from "./Pages/WorkshopPresenterDashboard/WorkshopPresenterDashboard";
+
 import Registration from "./Pages/Resregistration/Registration";
+import Login from "./Pages/Login/Login";
+import PaymentMethod from "./Pages/Payment/PaymentMethod";
+
 
 function App() {
-    return ( <
-        BrowserRouter > { /* <Navbar/> */ } <
-        Switch >
-        <
-        Route path = "/admin" >
-        <
-        AdminDashboard / >
-        <
-        /Route>
 
-        <
-        Route path = "/editor" >
-        <
-        EditorDashboard / >
-        <
-        /Route>
+    return (
 
-        <
-        Route path = "/reviewer" >
-        <
-        ReviewerDashboard / >
-        <
-        /Route>
+        <BrowserRouter>
 
-        <
-        Route path = "/researcher" >
-        <
-        ResearcherDashboard / >
-        <
-        /Route>
+            {/*<Login/>*/}
 
-        <
-        Route path = "/workshopPresenter" >
-        <
-        WorkshopPresenterDashboard / >
-        <
-        /Route>
+            <PaymentMethod/>
 
-        <
-        Route path = "/registration" >
-        <
-        Registration / >
-        <
-        /Route>
+            {/*<Switch>*/}
 
-        <
-        /Switch> <
-        /BrowserRouter>
+            {/*    <Route path="/admin">*/}
+
+            {/*        <AdminDashboard/>*/}
+
+            {/*    </Route>*/}
+
+            {/*    <Route path="/editor">*/}
+
+            {/*        <EditorDashboard/>*/}
+
+            {/*    </Route>*/}
+
+            {/*    <Route path="/reviewer">*/}
+
+            {/*        <ReviewerDashboard/>*/}
+
+            {/*    </Route>*/}
+
+            {/*    <Route path="/researcher">*/}
+
+            {/*        <ResearcherDashboard/>*/}
+
+            {/*    </Route>*/}
+
+            {/*    <Route path="/workshopPresenter">*/}
+
+            {/*        <WorkshopPresenterDashboard/>*/}
+
+            {/*    </Route>*/}
+
+            {/*    <div>*/}
+
+            {/*        <Navbar/>*/}
+
+            {/*        <Route path="/registration">*/}
+
+            {/*            <Registration/>*/}
+
+            {/*        </Route>*/}
+
+            {/*        <Route path="/login">*/}
+
+            {/*            <Login/>*/}
+
+            {/*        </Route>*/}
+
+            {/*        <Route path={"/paymentMethod"}>*/}
+
+            {/*            <PaymentMethod/>*/}
+
+            {/*        </Route>*/}
+
+
+            {/*    </div>*/}
+
+            {/*    <Route path={"/paymentMethod"}>*/}
+
+            {/*        <PaymentMethod/>*/}
+
+            {/*    </Route>*/}
+
+            {/*</Switch>*/}
+
+        </BrowserRouter>
+
     );
+
 }
+
 
 export default App;
