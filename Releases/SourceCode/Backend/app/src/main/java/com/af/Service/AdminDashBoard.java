de7c83d2c631c461ff302cbe9a245fad7736dcd9
tlorence <tirone.lorence@gmail.com>
2021-06-30 21:06:25 +0530

    AdminDashBoard services

---
+++ Releases/SourceCode/Backend/app/src/main/java/com/af/Service/AdminDashBoard.java
@@ -0,0 +1,6 @@
+package com.af.Service;
+
+public interface AdminDashBoard {
+
+//    List<>
+}
---
+++ Releases/SourceCode/Backend/app/src/main/java/com/af/Service/Impl/AdminDashBoardImpl.java
@@ -0,0 +1,11 @@
+package com.af.Service.Impl;
+
+import com.af.Service.AdminDashBoard;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AdminDashBoardImpl implements AdminDashBoard {
+//    List getPending
+}
